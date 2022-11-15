b9f1bd0b277fdeaf6612203d5a731800a550f091
Amit Yadav <amit375395yadav@gmail.com>
2022-11-15 16:33:44 +0530

    added l2 complete

---
+++ L2/2-3.py
@@ -0,0 +1,7 @@
+import keyword
+print('and is a keyword :', keyword.iskeyword('and'))
+
+#Fill in the missing code in the below lines
+print('exec is a keyword :',keyword.iskeyword('exec') )
+print('nonlocal is a keyword :',keyword.iskeyword('nonlocal') )
+print('False is a keyword :',keyword.iskeyword('False') )
---
+++ L2/3.py
@@ -0,0 +1,17 @@
+def add(a, b):
+	"""Return sum of given arguments."""
+	return a + b
+
+def power(b, e):
+	"""Return the power value.
+	
+	b -- is the base
+	e -- is the exponent
+	"""
+	return b ** e
+
+# print docstring of add method
+print(add.__doc__)
+
+# print docstring of power method
+print(power.__doc__)
