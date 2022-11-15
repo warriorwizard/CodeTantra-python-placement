c56d25e4755cc5cc95b78ca43e8caca8601c2734
Amit Yadav <amit375395yadav@gmail.com>
2022-11-15 16:38:35 +0530

    added L3

---
+++ L3/3.py
@@ -0,0 +1,11 @@
+value1 = value2 = value3 = "Hello"
+print(value1)
+print(value2)
+print(value3)
+# Assign values to variables and print again
+value1=99
+value2='Hello Python'
+value3='Hello World'
+print(value1)
+print(value2)
+print(value3)
