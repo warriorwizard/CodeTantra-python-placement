44abd7b48a16af2b364f1ddab7266e6cda152afe
DevilANANDGupta <ag075261@gmail.com>
2022-11-15 23:10:20 +0530

    absc

---
+++ L11/Logical Operators/Q2.py
@@ -0,0 +1,6 @@
+a=(input("M or F: "))
+b=int(input("age: "))
+if b%2==0:
+	print("Eligible for Concession")
+else:
+	print("Not Eligible for Concession")
---
+++ L11/Logical Operators/Q3.py
@@ -0,0 +1,10 @@
+# write your code here
+a=int(input("a: "))
+b=int(input("b: "))
+c=int(input("c: "))
+
+if a==6 and b==6 and c!=6:
+	
+	print(True)
+else:
+	print(False)
