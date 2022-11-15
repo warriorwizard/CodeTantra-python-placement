06226b0b90180f57e99290643892a186be06c70e
Amit Yadav <amit375395yadav@gmail.com>
2022-11-15 23:19:41 +0530

    removed 13

--- L13/4.py
+++
@@ -1,6 +0,0 @@
-value1=999
-value2=24.789
-value3='Python Interpreter'
-print(value1)
-print(value2)
-print(value3)
--- L13/5.py
+++
@@ -1,9 +0,0 @@
-str = input("Enter a value: ")
-# Assign str to three objects a, b and c
-# Print a
-# Print b
-# Print c
-a=b=c=str
-print('Value of a:',a)
-print('Value of b:',b)
-print('Value of c:',c)
