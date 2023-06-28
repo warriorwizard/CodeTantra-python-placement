4265439d0eef8bbf94f018a7a5345739506ed477
Sohan-2001 <112119230+Sohan-2001@users.noreply.github.com>
2023-06-28 17:30:02 +0530

    Update 2.py

--- L51/2.py
+++ L51/2.py
@@ -1,10 +1,5 @@
 def largestinthree(a, b, c):
-	if a>b and a>c:
-		return a
-	elif b>a and b>c:
-		return b
-	else:
-		return c
+	return max(a,max(b,c))
 	# write your code here to find the largest number in a, b and c
 		
 num1 = int(input("Please enter a value for num1: "))
